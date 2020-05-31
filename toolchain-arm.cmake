e2850c443c6855df1a9c30211c5d9671d0f7452a
Christoph Rothermel <10477273+Rodiii@users.noreply.github.com>
2020-05-31 21:33:05 +0200

    feat: add more precise rpi link

--- toolchain-arm.cmake
+++ toolchain-arm.cmake
@@ -3,17 +3,16 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # Specify the cross compiler
-set(tools ${PROJECT_SOURCE_DIR}/../gcc-linaro-arm-linux-gnueabihf-tools)
+# clone rpi tools from https://github.com/raspberrypi/tools.git
+set(tools <path-to-raspberrypi-tools>/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf)
 SET(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
 
 # Where is the target environment
-SET(CMAKE_FIND_ROOT_PATH ${tools}/arm-linux-gnueabihf)
+SET(CMAKE_FIND_ROOT_PATH ${tools}/arm-linux-gnueabihf/sysroot/)
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
 SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
 SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} --sysroot=${CMAKE_FIND_ROOT_PATH}")
-
-set(CMAKE_PREFIX_PATH ${tools}/arm-linux-gnueabihf/lib/arm-linux-gnueabihf/cmake)
 
 # Search for programs only in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
